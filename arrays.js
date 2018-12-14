71fa5d1cd9b3d2432982812b4316f3aa4803ecb1
Howard Lin <howardlin84@gmail.com>
2018-12-14 19:58:48 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,43 +1,115 @@
-var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
+// pushvar chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
 
-function addElementToBeginningOfArray(array,element){
-  return [element, ...array]
+// function addElementToBeginningOfArray(array,element){
+//   return [element, ...array]
+// }
+
+// var array = [1]
+
+// function destructivelyAddElementToBeginningOfArray(array, foo){
+//   array.unshift(foo)
+//   return array
+// }
+
+// function addElementToEndOfArray(array, foo){
+//   return [...array, foo]
+// }
+
+// function destructivelyAddElementToEndOfArray(array, element){
+//   array.push(element)
+//   return array
+// }
+
+// function accessElementInArray(array, index){
+//   return array[index]
+// }
+
+// function destructivelyRemoveElementFromBeginningOfArray(array, element){
+//   array.shift()
+//   return array
+// }
+
+// function removeElementFromBeginningOfArray(array){
+//   return array.slice(1)
+// }
+
+// function destructivelyRemoveElementFromEndOfArray(array){
+//   array.pop()
+//   return array
+// }
+
+// function removeElementFromEndOfArray(array){
+//   return array.slice(0, array.length - 1)
+// }
+
+var chocolateBars = ['snickers' , 'hundred grand' , 'kitkat', 'skittles'];
+
+
+function addElementToBeginningOfArray(array, element) {
+  return [element, ...array];
+}  
+
+function destructivelyAddElementToBeginningOfArray(array, element) {
+  array.unshift(element);
+  return array;
 }
 
-var array = [1]
-
-function destructivelyAddElementToBeginningOfArray(array, foo){
-  array.unshift(foo)
-  return array
+function addElementToEndOfArray(array, element) {
+  return [...array, element];
 }
 
-function addElementToEndOfArray(array, foo){
-  return [...array, foo]
+function destructivelyAddElementToEndOfArray(array, element) {
+  array.push(element);
+  return array;
 }
 
-function destructivelyAddElementToEndOfArray(array, element){
-  array.push(element)
-  return array
+function accessElementInArray(array, index) {
+  return array[index];
 }
 
-function accessElementInArray(array, index){
-  return array[index]
+function destructivelyRemoveElementFromBeginningOfArray(array) {
+  array.shift();
+  return array;
 }
 
-function destructivelyRemoveElementFromBeginningOfArray(array, element){
-  array.shift()
-  return array
+function removeElementFromBeginningOfArray(array) {
+  return array.slice(1);
 }
 
-function removeElementFromBeginningOfArray(array){
-  return array.slice(1)
+function destructivelyRemoveElementFromEndOfArray(array) {
+  array.pop();
+  return array;
 }
 
-function destructivelyRemoveElementFromEndOfArray(array){
-  array.pop()
-  return array
+function removeElementFromEndOfArray(array) {
+  return array.slice(0, array.length - 1);
 }
 
-function removeElementFromEndOfArray(array){
-  return array.slice(0, array.length - 1)
-}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
